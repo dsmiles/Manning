5c6285b0a4d6d2d236b7b73c0c68daa3742f79a1
dsmiles <dsmiles@users.noreply.github.com>
2023-09-18 18:40:32 +0100

    Update BugsTest.java
    Refactored test class and  introduce some additional helper methods

--- BugsTest.java
+++ BugsTest.java
@@ -3,6 +3,8 @@ package com.assetco.hotspots.optimization;
 import com.assetco.search.results.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,20 +13,20 @@ import static com.assetco.search.results.HotspotKey.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BugsTest {
-
+    final int maximumShowcaseItems = 5;
+    private AssetVendor partnerVendor;
     private SearchResults searchResults;
     private SearchResultHotspotOptimizer optimizer;
 
     @BeforeEach
     public void setup() {
+        partnerVendor = makeVendor(Partner);
         searchResults = new SearchResults();
         optimizer = new SearchResultHotspotOptimizer();
     }
 
     @Test
     public void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
-        final int maximumShowcaseItems = 5;
-        final var partnerVendor = makeVendor(Partner);
         var missing = givenAssetInResultsWithVendor(partnerVendor);
         givenAssetInResultsWithVendor(makeVendor(Partner));
         var expected = givenAssetsInResultsWithVendor(maximumShowcaseItems - 1, partnerVendor);
@@ -35,29 +37,26 @@ public class BugsTest {
         thenHotspotHasExactly(Showcase, expected);
     }
 
-    private AssetVendor makeVendor(AssetVendorRelationshipLevel relationshipLevel) {
-        var id = Any.string();
-        var displayName = Any.string();
-        var royaltyRate = Any.anyLong();
-
-        return new AssetVendor(id, displayName, relationshipLevel, royaltyRate);
+    private AssetVendor makeVendor(AssetVendorRelationshipLevel level) {
+        return new AssetVendor("anything", "anything", level, 1);
     }
 
+
     private Asset givenAssetInResultsWithVendor(AssetVendor vendor) {
-        var id = Any.string();
-        var title = Any.string();
-        var thumbnailURI = Any.URI();
-        var previewURI = Any.URI();
-        var last30Days = Any.assetPurchaseInfo();
-        var last24Hours = Any.assetPurchaseInfo();
-        var topics = new ArrayList<AssetTopic>();
-        topics.add(Any.anyTopic());
-        topics.add(Any.anyTopic());
-
-        var asset = new Asset(id, title, thumbnailURI, previewURI, last30Days, last24Hours, topics, vendor);
+        Asset asset = getAsset(vendor);
         searchResults.addFound(asset);
-
         return asset;
+    }
+
+    private Asset getAsset(AssetVendor vendor) {
+        return new Asset("anything", "anything", null, null, getPurchaseInfo(), getPurchaseInfo(),
+                new ArrayList<>(), vendor);
+    }
+
+    private AssetPurchaseInfo getPurchaseInfo() {
+        return new AssetPurchaseInfo(0, 0,
+                new Money(new BigDecimal("0")),
+                new Money(new BigDecimal("0")));
     }
 
     private ArrayList<Asset> givenAssetsInResultsWithVendor(int count, AssetVendor vendor) {
